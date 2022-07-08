1ca6e32802787dfb9a6bc7139a9c7faa80cdc24a
Hoid <tylerlcheek@gmail.com>
2022-07-08 11:23:05 -0400

    Fix multiline string and add debugging statements

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -1,17 +1,19 @@
 def build(List<String> destinationsList) {
     echo "Build stage"
+    echo "${destinationsList}"
 
     destinationsCmdSnippet = ""
     for (destination in destinationsList) {
         newDestination = " --destination " + destination + " "
         destinationsCmdSnippet += newDestination
     }
-    sh '''
+    echo "${destinationsCmdSnippet}"
+    sh """
         /kaniko/executor --dockerfile ./Dockerfile \
                         --context . \
                         --verbosity debug \
                         --no-push \
                         ${destinationsCmdSnippet} \
                         --tarPath image.tar
-    '''
+    """
 }

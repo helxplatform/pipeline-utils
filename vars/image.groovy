562ce439a7ec8f78423104347a540293f0273b19
Hoid <tylerlcheek@gmail.com>
2022-07-09 08:17:07 -0400

    Replace sh logic with just print statements

--- vars/image.groovy
+++ vars/image.groovy
@@ -15,28 +15,33 @@ def publish(config) {
         tagsPushForMasterBranchCmd += cmd
     }
     sh """
-        echo "Publish stage"
-        echo "${config.registryPsw}" | crane auth login -u ${config.registryUser} --password-stdin ${config.registry}
-        ${tagsPushAlwaysCmd}
-        if [ ${config.branchName} == "develop" ]; then
-            ${tagsPushForDevelopBranchCmd}
-        elif [ ${config.branchName} == "master" ]; then
-            ${tagsPushForMasterBranchCmd}
-            if [ $(git tag -l "${config.version}") ]; then
-                error "ERROR: Tag with version ${config.version} already exists! Exiting."
-            else
-                # Recover some things we've lost since the build stage:
-                git config --global user.email "helx-dev@lists"
-                git config --global user.name "rencibuild rencibuild"
-                grep url .git/config
-                git checkout ${config.branchName}
-
-                # Set the tag
-                SHA=$(git log --oneline | head -1 | awk '{print $1}')
-                git tag ${config.version} ${config.commitHash}
-                git remote set-url origin ${repoRemoteUrl}
-                git push origin --tags
-            fi
-        fi
+        echo "${tagsPushAlwaysCmd}"
+        echo "${tagsPushForDevelopBranchCmd}"
+        echo "${tagsPushForMasterBranchCmd}"
     """
+    // sh """
+    //     echo "Publish stage"
+    //     echo "${config.registryPsw}" | crane auth login -u ${config.registryUser} --password-stdin ${config.registry}
+    //     ${tagsPushAlwaysCmd}
+    //     if [ ${config.branchName} == "develop" ]; then
+    //         ${tagsPushForDevelopBranchCmd}
+    //     elif [ ${config.branchName} == "master" ]; then
+    //         ${tagsPushForMasterBranchCmd}
+    //         if [ $(git tag -l "${config.version}") ]; then
+    //             error "ERROR: Tag with version ${config.version} already exists! Exiting."
+    //         else
+    //             # Recover some things we've lost since the build stage:
+    //             git config --global user.email "helx-dev@lists"
+    //             git config --global user.name "rencibuild rencibuild"
+    //             grep url .git/config
+    //             git checkout ${config.branchName}
+
+    //             # Set the tag
+    //             SHA=$(git log --oneline | head -1 | awk '{print $1}')
+    //             git tag ${config.version} ${config.commitHash}
+    //             git remote set-url origin ${repoRemoteUrl}
+    //             git push origin --tags
+    //         fi
+    //     fi
+    // """
 }

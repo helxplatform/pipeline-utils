84712517f2e04549c06c49b3ce8492db3a92f8ce
Hoid <tylerlcheek@gmail.com>
2022-07-09 07:46:15 -0400

    Restore full shell script

--- vars/image.groovy
+++ vars/image.groovy
@@ -14,11 +14,29 @@ def publish(config) {
         cmd = "crane push image.tar " + tag + "; "
         tagsPushForMasterBranchCmd += cmd
     }
-    println ${config.branchName}
-    println ${tagsPushForMasterBranchCmd}
     sh """
         echo "Publish stage"
         echo "${config.registryPsw}" | crane auth login -u ${config.registryUser} --password-stdin ${config.registry}
         ${tagsPushAlwaysCmd}
+        if [ ${config.branchName} == "develop" ]; then
+            ${tagsPushForDevelopBranchCmd}
+        elif [ ${config.branchName} == "master" ]; then
+            ${tagsPushForMasterBranchCmd}
+            if [ $(git tag -l "${config.version}") ]; then
+                error "ERROR: Tag with version ${config.version} already exists! Exiting."
+            else
+                # Recover some things we've lost since the build stage:
+                git config --global user.email "helx-dev@lists"
+                git config --global user.name "rencibuild rencibuild"
+                grep url .git/config
+                git checkout ${config.branchName}
+
+                # Set the tag
+                SHA=$(git log --oneline | head -1 | awk '{print $1}')
+                git tag ${config.version} ${config.sha}
+                git remote set-url origin ${repoRemoteUrl}
+                git push origin --tags
+            fi
+        fi
     """
 }

e0c6792e601e84fbf7fdd491cd68eab23c002f56
Hoid <tylerlcheek@gmail.com>
2022-07-11 09:48:05 -0400

    Stop using config map parameter

--- vars/image.groovy
+++ vars/image.groovy
@@ -1,18 +1,18 @@
-def publish(config) {
+def publish(imageTagsPushAlways = [], imageTagsPushForDevelopBranch = [], imageTagsPushForMasterBranch = []) {
     String tagsPushAlwaysCmd = ""
     String tagsPushForDevelopBranchCmd = ""
     String tagsPushForMasterBranchCmd = ""
-    for (tag in config.imageTagsPushAlways) {
+    for (tag in imageTagsPushAlways) {
         if (containsIllegalCharacter(tag)) { return }
         cmd = "crane push image.tar " + tag + "; "
         tagsPushAlwaysCmd += cmd
     }
-    for (tag in config.imageTagsPushForDevelopBranch) {
+    for (tag in imageTagsPushForDevelopBranch) {
         if (containsIllegalCharacter(tag)) { return }
         cmd = "crane push image.tar " + tag + "; "
         tagsPushForDevelopBranchCmd += cmd
     }
-    for (tag in config.imageTagsPushForMasterBranch) {
+    for (tag in imageTagsPushForMasterBranch) {
         if (containsIllegalCharacter(tag)) { return }
         cmd = "crane push image.tar " + tag + "; "
         tagsPushForMasterBranchCmd += cmd

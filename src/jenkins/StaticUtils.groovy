d8bfd548a1668efd1e15627b809310c9ba2918fa
Hoid <tylerlcheek@gmail.com>
2022-07-13 10:01:54 -0400

    Fix bug with shell cmd usage in StaticUtils

--- src/jenkins/StaticUtils.groovy
+++ src/jenkins/StaticUtils.groovy
@@ -4,7 +4,7 @@ class StaticUtils implements Serializable {
 
     static boolean containsIllegalCharacter(String input) {
     if (input.contains(';') || input.contains('&')) {
-        sh("echo 'An input string contains either ';' or '&', which is not allowed. Exiting...'"
+        sh "echo 'An input string contains either ';' or '&', which is not allowed. Exiting...'"
         return true
     } else {
         return false

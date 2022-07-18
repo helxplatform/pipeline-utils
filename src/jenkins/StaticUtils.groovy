fc427bcd5b1f20ba9b3dcf97b760e576daa6971c
Hoid <tylerlcheek@gmail.com>
2022-07-18 09:59:29 -0400

    Initial commit

--- src/jenkins/StaticUtils.groovy
+++ src/jenkins/StaticUtils.groovy
@@ -3,12 +3,16 @@ package jenkins
 class StaticUtils implements Serializable {
 
     static boolean containsIllegalCharacter(String input) {
-    if (input.contains(';') || input.contains('&')) {
-        sh "echo 'An input string contains either ';' or '&', which is not allowed. Exiting...'"
-        return true
-    } else {
-        return false
+        if (input.contains(';') || input.contains('&')) {
+            sh "echo 'An input string contains either ';' or '&', which is not allowed. Exiting...'"
+            return true
+        } else {
+            return false
+        }
     }
-}
+
+    static transformSlashToDash(String input) {
+        return input.replaceAll("/", "-")
+    }
 
 }

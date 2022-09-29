883972f975a3e2fab5373c4d873bd0edc28c32ed
Hoid <tylerlcheek@gmail.com>
2022-09-29 12:59:48 -0400

    Add echo statement for debugging

--- vars/go.groovy
+++ vars/go.groovy
@@ -1,6 +1,7 @@
 def ccv(String repoRemoteUrl, String repoName) {
     withEnv(["REPO_REMOTE_URL=repoRemoteUrl", "REPO_NAME=repoName"]) {
         sh '''
+        echo ${REPO_NAME}
         git clone ${REPO_REMOTE_URL}
         cd ${REPO_NAME}
         go install github.com/smlx/ccv@v0.3.2

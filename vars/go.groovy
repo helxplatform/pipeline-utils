38de04701d3e234beab0ec37d9df1dab46977c68
Hoid <tylerlcheek@gmail.com>
2022-09-29 11:59:53 -0400

    Add github pull/push and change go command

--- vars/go.groovy
+++ vars/go.groovy
@@ -1,15 +1,21 @@
-def ccv() {
-    sh '''
-    go get github.com/smlx/ccv
-    cat <<EOF > ccv.sh
-    #!/bin/bash
-    if [ -z $(git tag -l $(ccv)) ]; then
-	    git tag $(ccv)
-        git tag   # testing
-        # git push --tags
-    fi
-    EOF
-    chmod +x ccv.sh
-    ./ccv.sh
-    '''
+def ccv(gitCredsUsr, gitCredsPsw) {
+    withEnv(["GIT_CREDS_USR=gitCredsUsr", "GIT_CREDS_PSW=gitCredsPsw"]) {
+        sh '''
+        git clone https://github.com/helxplatform/helx-ui.git
+        cd helx-ui/
+        go install github.com/smlx/ccv@v0.3.2
+        cat <<EOF > ccv.sh
+        #!/bin/bash
+        if [ -z $(git tag -l $(ccv)) ]; then
+            git tag $(ccv)
+            git tag   # testing
+            git remote set origin https://${GIT_CREDS_USR}:${GIT_CREDS_PSW}@github.com/helxplatform/helx-ui.git
+            # git push --tags
+            # git push origin --tags
+        fi
+        EOF
+        chmod +x ccv.sh
+        ./ccv.sh
+        '''
+    }
 }

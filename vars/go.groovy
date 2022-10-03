e8d449ac608389827515d7f1601d5097fb4c78f1
Hoid <tylerlcheek@gmail.com>
2022-10-03 10:20:33 -0400

    Take out debugging and finish up

--- vars/go.groovy
+++ vars/go.groovy
@@ -5,13 +5,10 @@ def ccv(String repoRemoteUrl, String repoName) {
         cd ${REPO_NAME}
         /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2
         TAG=$(/go/bin/ccv)
-        echo $TAG
         if [ -z $(git tag -l $TAG) ]; then
             git tag $TAG
-            git tag   # testing
             git remote set origin ${REPO_REMOTE_URL}
-            # git push --tags
-            # git push origin --tags
+            git push origin --tags
         fi
         '''
     }

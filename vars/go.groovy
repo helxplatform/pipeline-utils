2ae41553c8ab27ac661505b5a70bf35c92444798
Hoid <tylerlcheek@gmail.com>
2022-10-04 14:12:59 -0400

    Redirect shell script stdout to groovy variable

--- vars/go.groovy
+++ vars/go.groovy
@@ -1,15 +1,16 @@
 def ccv(String repoRemoteUrl, String repoName) {
     withEnv(["REPO_REMOTE_URL=${repoRemoteUrl}", "REPO_NAME=${repoName}"]) {
-        sh '''
-        git clone ${REPO_REMOTE_URL}
-        cd ${REPO_NAME}
-        /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2
-        TAG=$(/go/bin/ccv)
-        if [ -z $(git tag -l $TAG) ]; then
-            git tag $TAG
-            git remote set origin ${REPO_REMOTE_URL}
-            git push origin --tags
-        fi
-        '''
+        return sh(returnStdout: true, script: '
+            git clone ${REPO_REMOTE_URL}; \
+            cd ${REPO_NAME}; \
+            /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2 > /dev/null 2>&1; \
+            TAG=$(/go/bin/ccv) > /dev/null 2>&1; \
+            if [ -z $(git tag -l $TAG > /dev/null 2>&1)]; then \
+                git tag $TAG > /dev/null 2>&1; \
+                git remote set origin ${REPO_REMOTE_URL}; \
+                git push origin --tags > /dev/null 2>&1; \
+            fi \
+            echo $TAG \
+        ')
     }
 }

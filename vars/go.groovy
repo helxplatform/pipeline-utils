05958c1623975def8266e631efc78921af6f2afe
Hoid <tylerlcheek@gmail.com>
2022-10-04 14:47:02 -0400

    Remove piping stdout to /dev/null from if

--- vars/go.groovy
+++ vars/go.groovy
@@ -3,12 +3,12 @@ def ccv(String repoRemoteUrl, String repoName) {
         return sh(returnStdout: true, script: '''
             git clone ${REPO_REMOTE_URL}
             cd ${REPO_NAME}
-            /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2 > /dev/null 2>&1
-            TAG=$(/go/bin/ccv) > /dev/null 2>&1
-            if [ -z $(git tag -l $TAG > /dev/null 2>&1)]; then
-                git tag $TAG > /dev/null 2>&1
+            /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2 > /dev/null
+            TAG=$(/go/bin/ccv) > /dev/null
+            if [ -z $(git tag -l $TAG)]; then
+                git tag $TAG > /dev/null
                 git remote set origin ${REPO_REMOTE_URL}
-                git push origin --tags > /dev/null 2>&1
+                git push origin --tags > /dev/null
             fi
             echo $TAG
         ''')

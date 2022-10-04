69fdce1edaf33f3154d6188856f109f011235888
Hoid <tylerlcheek@gmail.com>
2022-10-04 14:23:30 -0400

    Try 3 apostrophes

--- vars/go.groovy
+++ vars/go.groovy
@@ -1,6 +1,6 @@
 def ccv(String repoRemoteUrl, String repoName) {
     withEnv(["REPO_REMOTE_URL=${repoRemoteUrl}", "REPO_NAME=${repoName}"]) {
-        return sh(returnStdout: true, script: '
+        return sh(returnStdout: true, script: '''
             git clone ${REPO_REMOTE_URL}; \
             cd ${REPO_NAME}; \
             /usr/local/go/bin/go install github.com/smlx/ccv@v0.3.2 > /dev/null 2>&1; \
@@ -11,6 +11,6 @@ def ccv(String repoRemoteUrl, String repoName) {
                 git push origin --tags > /dev/null 2>&1; \
             fi \
             echo $TAG \
-        ')
+        ''')
     }
 }

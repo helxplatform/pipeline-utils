59202fc81d2a96ad8e6184168446103ddf6a09b7
Hoid <tylerlcheek@gmail.com>
2022-10-10 11:22:54 -0400

    Update image.groovy to remove reference to VERSION

--- vars/image.groovy
+++ vars/image.groovy
@@ -35,21 +35,6 @@ def publish(imageTagsToPushAlways = [], imageTagsToPushForDevelopBranch = [], im
             $tagsToPushForDevelopBranchCmd
         elif [ \$BRANCH_NAME == "master" ]; then
             $tagsToPushForMasterBranchCmd
-            if [ \$(git tag -l "\$VERSION") ]; then
-                error "ERROR: Tag with version \$VERSION already exists! Exiting."
-            else
-                # Recover some things we've lost since the build stage:
-                git config --global user.email "helx-dev@lists"
-                git config --global user.name "rencibuild rencibuild"
-                grep url .git/config
-                git checkout \$BRANCH_NAME
-
-                # Set the tag
-                SHA=\$(git log --oneline | head -1 | awk '{print \$1}')
-                git tag \$VERSION \$COMMIT_HASH
-                git remote set-url origin \$REPO_REMOTE_URL
-                git push origin --tags
-            fi
         fi
     """
 }

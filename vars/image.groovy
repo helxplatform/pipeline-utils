5d9d3626735c6d441f3c8e53c232e9f8b7283e56
Hoid <tylerlcheek@gmail.com>
2022-10-10 11:26:08 -0400

    Make new publish2 versioned method until all projects are migrated

--- vars/image.groovy
+++ vars/image.groovy
@@ -1,5 +1,44 @@
 import jenkins.StaticUtils
 
+def publish2(imageTagsToPushAlways = [], imageTagsToPushForDevelopBranch = [], imageTagsToPushForMasterBranch = []) {
+    String tagsToPushAlwaysCmd = ''
+    String tagsToPushForDevelopBranchCmd = ''
+    String tagsToPushForMasterBranchCmd = ''
+    for (tag in imageTagsToPushAlways) {
+        if (StaticUtils.containsIllegalCharacter(tag)) { return }
+        tag = StaticUtils.transformSlashToUnderscoreInTag(tag)
+        cmd = 'crane push image.tar ' + tag + '; '
+        tagsToPushAlwaysCmd += cmd
+    }
+    for (tag in imageTagsToPushForDevelopBranch) {
+        if (StaticUtils.containsIllegalCharacter(tag)) { return }
+        tag = StaticUtils.transformSlashToUnderscoreInTag(tag)
+        cmd = 'crane push image.tar ' + tag + '; '
+        tagsToPushForDevelopBranchCmd += cmd
+    }
+    for (tag in imageTagsToPushForMasterBranch) {
+        if (StaticUtils.containsIllegalCharacter(tag)) { return }
+        tag = StaticUtils.transformSlashToUnderscoreInTag(tag)
+        cmd = 'crane push image.tar ' + tag + '; '
+        tagsToPushForMasterBranchCmd += cmd
+    }
+    // Using string interpolation is fine for plaintext variables, but never
+    // use it for secrets. Those secrets can be unwittingly logged to the 
+    // console. See here for more: 
+    // https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#injection-via-interpolation
+    // Also, make sure to escape the $ on any new shell environment variable used here.
+    sh """
+        echo "Publish stage"
+        echo "\$DOCKERHUB_CREDS_PSW" | crane auth login -u \$DOCKERHUB_CREDS_USR --password-stdin \$REGISTRY
+        $tagsToPushAlwaysCmd
+        if [ \$BRANCH_NAME == "develop" ]; then
+            $tagsToPushForDevelopBranchCmd
+        elif [ \$BRANCH_NAME == "master" ]; then
+            $tagsToPushForMasterBranchCmd
+        fi
+    """
+}
+
 def publish(imageTagsToPushAlways = [], imageTagsToPushForDevelopBranch = [], imageTagsToPushForMasterBranch = []) {
     String tagsToPushAlwaysCmd = ''
     String tagsToPushForDevelopBranchCmd = ''
@@ -35,6 +74,17 @@ def publish(imageTagsToPushAlways = [], imageTagsToPushForDevelopBranch = [], im
             $tagsToPushForDevelopBranchCmd
         elif [ \$BRANCH_NAME == "master" ]; then
             $tagsToPushForMasterBranchCmd
+            # Recover some things we've lost since the build stage:
+            git config --global user.email "helx-dev@lists"
+            git config --global user.name "rencibuild rencibuild"
+            grep url .git/config
+            git checkout \$BRANCH_NAME
+
+            # Set the tag
+            SHA=\$(git log --oneline | head -1 | awk '{print \$1}')
+            git tag \$VERSION \$COMMIT_HASH
+            git remote set-url origin \$REPO_REMOTE_URL
+            git push origin --tags
         fi
     """
 }

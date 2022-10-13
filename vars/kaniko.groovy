b5d59e3c6b736b5e4b37e9a6df70042e85b94bfb
Hoid <tylerlcheek@gmail.com>
2022-10-13 15:54:12 -0400

    Add buildAndPush() method for kaniko

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -28,3 +28,30 @@ def build(String pathToDockerfile, List<String> destinationsList) {
                         --tarPath image.tar
     """
 }
+
+/*
+Builds a kaniko image and pushes it to harbor, given a path to a dockerfile and a list of image tags to use as destinations.
+ */
+def buildAndPush(String pathToDockerfile, List<String> destinationsList) {
+    echo "Build stage"
+
+    if (StaticUtils.containsIllegalCharacter(pathToDockerfile)) { return }
+
+    String destinationsCmdSnippet = ""
+    for (destination in destinationsList) {
+        if (StaticUtils.containsIllegalCharacter(destination)) { return }
+        destination = StaticUtils.transformSlashToUnderscoreInTag(destination)
+        singleDestinationCmdSnippet = " --destination " + destination + " "
+        destinationsCmdSnippet += singleDestinationCmdSnippet
+    }
+    // Using string interpolation is fine for plaintext variables, but never
+    // use it for secrets. Those secrets can be unwittingly logged to the 
+    // console. See here for more: 
+    // https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#injection-via-interpolation
+    sh """
+        /kaniko/executor --dockerfile $pathToDockerfile \
+                        --context . \
+                        --verbosity debug \
+                        $destinationsCmdSnippet
+    """
+}
